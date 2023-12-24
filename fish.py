ed610f1a5aeccf2a39daeeaa8d32c01f165fd39e
anonimtester <154726917+anonimtester@users.noreply.github.com>
2023-12-24 12:25:21 +0400

    fish.py

--- fish.py
+++ fish.py
@@ -1,14 +1,18 @@
 import requests
 
-sites = str(input("Введи сайт: "))
+try:
+	sites = str(input("Введи сайт: "))
 
-links = requests.get(f'https://{sites}')
-response = links.status_code
-print("Status: ", response)
+	links = requests.get(f'https://{sites}')
+	response = links.status_code
+	print("Status: ", response)
 
-if response == 200:
-	with open(f'{sites}.html', 'w', encoding='utf-8') as file:
-		file.write(links.text)
-		print("Страница f'{sites}' сохранена")
-else:
-	print("Попробуй другую страницу")
+
+	if response == 200:
+		with open(f'{sites}.html', 'w', encoding='utf-8') as file:
+			file.write(links.text)
+			print(f'Страница {sites} сохранена')
+	else:
+		print("Попробуй другую страницу")
+except:
+	print("Произошла ошибка")
